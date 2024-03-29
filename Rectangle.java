b2fc6af894b8ecf3331b482831fe11862e27f77e
elijahjulian06 <julianer@appstate.edu>
2024-03-29 10:04:54 -0400

    this is for freddy

---
+++ Rectangle.java
@@ -0,0 +1,9 @@
+
+
+//private vars named length and width
+//public var called color
+//getters and setters for both length and width
+//print method that prints the length,width,color
+//user input -> that ask for the length, width, color
+//main should make a square object, set length, width, and color by calling user input method.
+//then print
