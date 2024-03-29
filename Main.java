8c245da79537f0643eac9e0e4724d6a1d2fc9cf0
Freddy Adongo <46581440+FreddyAdongo@users.noreply.github.com>
2024-03-29 10:46:25 -0400

    Rectangle class

--- Main.java
+++ Main.java
@@ -1,66 +1,95 @@
+import java.util.Scanner;
+
 public class Main
 {
 
 
-public boolean isPrime(int n)
-{
-    //n is prime if it divisible by only its self and one.
-    //we know n is diviible by itself
-    //we know its divible by 1 
-    // so we check whats between 1 and n
-    //
-    //
-    //loop throught ever single variable less then 7 and greater or equal to 2 
+// public boolean isPrime(int n)
+// {
+//     //n is prime if it divisible by only its self and one.
+//     //we know n is diviible by itself
+//     //we know its divible by 1 
+//     // so we check whats between 1 and n
+//     //
+//     //
+//     //loop throught ever single variable less then 7 and greater or equal to 2 
 
-    for(int i = 2; i < n ; i++)
-    {
-        //
-        if(n % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
+//     for(int i = 2; i < n ; i++)
+//     {
+//         //
+//         if(n % i == 0)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
 
-}
-// palidrome checker
-// takes in a string
-// and return true if it is a palidrome -> else false.
-public boolean palidromeChecker(String p)
-{
+// }
+// // palidrome checker
+// // takes in a string
+// // and return true if it is a palidrome -> else false.
+// public boolean palidromeChecker(String p)
+// {
     
-    //need a for loop
-    //if statements inside for loop. if index is equal to another.
-    //if it is not equal we return false. else its true 
-    if(p.isEmpty() == true)
-    {
-      return false;
-    }
+//     //need a for loop
+//     //if statements inside for loop. if index is equal to another.
+//     //if it is not equal we return false. else its true 
+//     if(p.isEmpty() == true)
+//     {
+//       return false;
+//     }
 
-    for(int i = 0, j = p.length() - 1; i < p.length(); i++, j--)
-    {
-        if(p.charAt(i) != p.charAt(j))
-        {
-            return false;
-        }
-    }
-    return true;
-}
+//     for(int i = 0, j = p.length() - 1; i < p.length(); i++, j--)
+//     {
+//         if(p.charAt(i) != p.charAt(j))
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// public static void main(String[] args)
+// {
+    
+//     //System.out.println("poop".length());
+//     Main main = new Main();
+//     /**
+//     System.out.println(main.palidromeChecker("101010101010101")); //false
+//     System.out.println(main.palidromeChecker("poop")); //true
+//     System.out.println(main.palidromeChecker("dog")); //false
+//     */
+//     System.out.println(main.isPrime(7)); //true
+//     System.out.println(main.isPrime(10)); //false
+//     System.out.println(main.isPrime(1));
+
+// }
 
 public static void main(String[] args)
 {
-    
-    //System.out.println("poop".length());
-    Main main = new Main();
-    /**
-    System.out.println(main.palidromeChecker("101010101010101")); //false
-    System.out.println(main.palidromeChecker("poop")); //true
-    System.out.println(main.palidromeChecker("dog")); //false
-    */
-    System.out.println(main.isPrime(7)); //true
-    System.out.println(main.isPrime(10)); //false
-    System.out.println(main.isPrime(1));
+    //main should make a rectangle object, then set the length, width, color with the constructor. Then print
+    //Then set them using the setters then print
+    //then set the using the user input method then print.
+    int length = 0;
+    int width = 0;
+    String color = "blue";
 
+    Rectangle rectangle = new Rectangle(length, width, color);
+
+    rectangle.setLength(6);
+    rectangle.setWidth(4);
+
+    rectangle.color = "pink";
+
+    Scanner input = new Scanner(System.in);
+
+    rectangle.userInput(input);
+
+
+    System.out.println(rectangle.toString());
+
+
+    
 }
 
 }
--- Rectangle.java
+++ Rectangle.java
@@ -1,9 +1,72 @@
-
-
 //private vars named length and width
 //public var called color
 //getters and setters for both length and width
 //print method that prints the length,width,color
 //user input -> that ask for the length, width, color
-//main should make a square object, set length, width, and color by calling user input method.
-//then print
+//main should make a rectangle object, then set the length, width, color with the constructor. Then print
+//Then set them using the setters then print
+//then set the using the user input method then print.
+
+import java.util.Scanner;
+
+public class Rectangle
+{
+    private int length;
+    private int width;
+
+    public String color;
+
+    public Rectangle()
+    {
+        System.out.print("no-arg Rectangle");
+    }
+
+    public Rectangle(int length, int width, String color)
+    {
+        this.length = length;
+        this.width = width;
+
+        this.color = color;
+    }
+
+    public void setLength(int length)
+    {
+        this.length = length;
+    }
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public String toString()
+    {
+        String str = "Length: " + length + " Width: " + width + " Colour: " + color;
+
+        return str;
+    }
+
+    public void userInput(Scanner input)
+    {
+        System.out.println("Enter a length: ");
+        length = input.nextInt();
+
+        System.out.println("Enter a width: ");
+        width = input.nextInt();
+
+        input.nextLine();
+        System.out.println("Enter a colour: ");
+        color = input.nextLine();
+    }
+
+}
